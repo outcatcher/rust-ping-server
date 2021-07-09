0f9d68e72a3d3f8c748e6b7c93f9d433af741c62
Anton Kachurin <anton.kachurin@t-systems.com>
2021-07-09 17:33:47 +0300

    Create hello-world http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+
+#[macro_use]
+extern crate rocket;
+
+use rocket::{Build, Rocket};
+
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/", routes![index])
+}
+
+#[rocket::main]
+async fn main() {
+    if let Err(e) = rocket().launch().await {
+        println!("Whoops! Rocket didn't launch!");
+        // We drop the error to get a Rocket-formatted panic.
+        drop(e);
+    };
 }
